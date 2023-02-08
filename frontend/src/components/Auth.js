c1addd3ca9a356e6448cafa82eec167fb9a0033a
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-02-09 00:42:27 +0100

    feature: guardar token en localStorage a través de un context

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -3,23 +3,34 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 export const Auth = () => {
-  const { loggedUser, logout} = useContext(AuthContext);
+  const { loggedUser, logout } = useContext(AuthContext);
   return loggedUser ? (
     <ul>
       <li>
-        Conectado como {loggedUser.username} 
+        <img
+          src={`./uploads/${loggedUser.photo}`}
+          alt="avatar"
+        /> 
+         {loggedUser.username}
       </li>
-      <li><button onClick={()=>{logout()}}>Salir</button></li>
+      <li>
+        <button
+          onClick={() => {
+            logout();
+          }}
+        >
+          Salir
+        </button>
+      </li>
     </ul>
-     ) : (
-    <ul>  
-      <li>      
+  ) : (
+    <ul>
+      <li>
         <Link to="/register">Registro</Link>
       </li>
       <li>
         <Link to="/users/login">Iniciar sesión</Link>
       </li>
-      {/* <li><Link to="/news/top">Top ranked news</Link></li> */}
     </ul>
   );
 };
