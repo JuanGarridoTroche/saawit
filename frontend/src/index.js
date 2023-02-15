0f62591631b8106fa2540cbea62a0acaacf509ed
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-02-15 17:13:13 +0100

    Actualizción

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProviderComponent } from "./context/AuthContext";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
