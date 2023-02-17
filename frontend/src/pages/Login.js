7977a27c72f1135f6604480e995cb1e8b24818fe
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-02-17 23:59:02 +0100

    actualizando el CSS de login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,46 +1,65 @@
-import { useContext, useState } from "react"
-import { useNavigate } from "react-router-dom";
+import "../css/login.css";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { loginUserService } from "../services";
 
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export const Login = ()=> {
-  const [email, setEmail] =useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const {login} = useContext(AuthContext);
-  const navigate = useNavigate(); 
-
-  const handleSubmit = async (e)=> {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
 
     try {
-      const data = await loginUserService({email, password});
+      const data = await loginUserService({ email, password });
       login(data.token);
       navigate("/");
-      
     } catch (error) {
-      setError(error.message)
+      setError(error.message);
     }
-
-  }
+  };
 
   return (
-    <section>
-      <h1>Iniciar sesión</h1>
+    <section className="login">
+      <h2>Iniciar sesión</h2>
       {error ? <p>{error}</p> : null}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">email </label>
-        <input id="email" name="email" type="email" placeholder="cuenta de correo" required onChange={(e) => {
-          setEmail(e.target.value)
-        }}/>
-        <label htmlFor="password">Contraseña </label>
-        <input id="password" name="password" type="password" required onChange={(e)=> {
-          setPassword(e.target.value)
-        }}/>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="cuenta de correo"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Contraseña"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <button>Enviar</button>
       </form>
+      <section className="dont-forget">
+        <p>¿Has olvidado tu nombre de usuario o contraseña ?</p>
+        <p>
+          ¿Es tu primera vez en Reddit?{" "}
+          <Link to="/register">
+            REGISTRARSE
+          </Link>
+        </p>
+      </section>
     </section>
-  )
-}
+  );
+};
